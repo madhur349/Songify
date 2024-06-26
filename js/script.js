3944a84cb23ee18c489d473f087f4d48685a89a7
madhur349 <128949115+madhur349@users.noreply.github.com>
2024-06-26 10:33:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ function formatTime(seconds) {
 async function getSongs(folder) {
     
 currfolder= folder;
-    let a=await fetch(`/${folder}/`)
+let a=await fetch(`http://127.0.0.1:5500/${folder}/`)
     let response= await a.text();
     console.log(response)
     let div = document.createElement("div")
@@ -84,7 +84,7 @@ currfolder= folder;
 }
 
 async function displayAlbums(){
-    let a=await fetch(`/songs/`)
+    let a=await fetch(`http://127.0.0.1:5500/songs/`)
     let response= await a.text();
     let div = document.createElement("div")
     div.innerHTML= response;
@@ -96,11 +96,11 @@ async function displayAlbums(){
         const element = array[index];
         
      
-        
+        let folders=[]
         if(element.href.includes("/songs/")&& !element.href.includes(".htaccess")){
            let folder=element.href.split("/").slice(-2)[1]
            
-            let a=await fetch(`/songs/${folder}/info.json`)
+           let a=await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
             let response= await a.json();
             cardContainer.innerHTML= cardContainer.innerHTML+`<div data-folder="${folder}" class="card">
             <div class="play">
@@ -108,7 +108,7 @@ async function displayAlbums(){
             </div>
             <img
               src="/songs/${folder}/cover.jpg"
-              alt="see image"
+              alt=""
             />
             <h2>${response.title}</h2>
             <p>${response.description}</p>
